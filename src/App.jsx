b702427d58de65c02057231bc016ee5ce06bc08d
Nahuel Bocanegra <nahuboca7@gmail.com>
2024-07-23 19:16:34 -0300

    fc

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import './assets/styles/styles.less'
+
+
+function App() {
+  
+  return (
+    
+    <div>
+
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+
+  )
+}
+
+export default App
