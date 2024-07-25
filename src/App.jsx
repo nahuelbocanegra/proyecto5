d5b8f18959ecb14cba4d4e28a50946d6c00d3adc
Nahuel Bocanegra <nahuboca7@gmail.com>
2024-07-24 22:40:54 -0300

    sc

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 import './assets/styles/styles.less'
+import img from './assets/img/js/img'
 
 
 function App() {
+  const cartas=[1,2,3,4,5,6,7,8,9,10,11,12]
+
+
+  
+
+
   
   return (
     
     <div>
 
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        {
+          cartas.map((num,index)=>(
+            <li key={index}>
+              <div className='front'>
+                  
+              </div>
+              <div className='back'>
+                {img.map((url,index)=>(
+                    <img src={url} alt="" srcset="" />
+                ))}
+                  
+              </div>
+            </li>
+          ))
+        }
+     
       </ul>
     </div>
 
---
+++ src/assets/img/js/img.js
@@ -0,0 +1,13 @@
+import cumple from "../cumple.png"
+import cumple1 from "../cumple(1).png"
+import cumple2 from "../cumple(2).png"
+import cumple3 from "../cumple(3).png"
+import cumple4 from "../cumple(4).png"
+import cumple5 from "../cumple(5).png"
+
+export default[cumple,cumple1,cumple2,cumple3,cumple4,cumple5,cumple,
+    cumple1,
+    cumple2,
+    cumple3,
+    cumple4,
+    cumple5]
