b35e15983a4537e18e96615b966a91a743dda618
Nahuel Bocanegra <nahuboca7@gmail.com>
2024-07-29 23:30:34 -0300

    Fc

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,24 @@ import img from './assets/img/js/img'
 
 function App() {
   const cartas=[img[0],img[1],img[2],img[3],img[4],img[5],img[6],img[7],img[8],img[9],img[10],img[11]]
-  
+  const [estado,setEstado]=useState(true);
 
+ 
+  const Li=(props)=>{
+    const{img,index}=props;
+
+    console.log(index)
+    return(
+      <li onClick={()=>{setEstado(!estado)}} className={`${estado? "":`estado${index}`}`}>
+              <div className='front'>
+                  
+              </div>
+              <div className='back'>
+                <img src={img} alt="" />
+              </div>
+      </li>
+    )
+  }
 
   
 
@@ -19,18 +35,9 @@ function App() {
 
       <ul>
         {
-          cartas.map((num,index)=>(
-            <li key={index}>
-              <div className='front'>
-                  
-              </div>
-              <div className='back'>
-                <img src={num} alt="" />
-               
-                
-                  
-              </div>
-            </li>
+          cartas.map((img,index)=>(
+           
+           <Li key={index} index={index} img={img}></Li>
           ))
         }
      
